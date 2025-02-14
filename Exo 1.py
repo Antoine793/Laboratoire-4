8bf0001b3f2cc14fadf347d0635c569c7f198474
Antoine793 <202272651@cegepsherbrooke.qc.ca>
2025-02-14 13:55:41 -0500

    Add files via upload

---
+++ Exo 1.py
@@ -0,0 +1,20 @@
+print ("Vous avez votre feuille dexamen devant vous. Vous avez 3 questions à répondre. Bonne chance !")
+print ("Question 1: Quel est le résultat de 3+7 ?")
+reponse1 = input()
+if reponse1 == "10":
+    print ("Bonne réponse !")
+else:
+    print ("Mauvaise réponse !")
+print ("Question 2: Quel est le résultat de 5*6 ?")
+reponse2 = input()
+if reponse2 == "30":
+    print ("Bonne réponse !")
+else:
+    print ("Mauvaise réponse !")
+print ("Question 3: Quel est le résultat de 8/2 ?")
+reponse3 = input()
+if reponse3 == "4":
+    print ("Bonne réponse !")
+else:
+    print ("Mauvaise réponse !")
+print ("Fin de lexamen !")
